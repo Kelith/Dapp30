fb7f511a77ebea208f239ae043f9c4342008101a
Kelith <garofalodavide7@gmail.com>
2022-05-15 09:52:17 +0200

    added notes

--- 7-split-payment/test/splitPayment.js
+++ 7-split-payment/test/splitPayment.js
@@ -9,6 +9,7 @@ contract('SplitPayment', (accounts) => {
   it('Should split payment', async () => {
     const recipients = [accounts[1], accounts[2], accounts[3]];
     const amounts = [40, 20, 30];
+    // promise.all awaits an array of promises at once. GetBalance returns an awaitable, we map them into an array of receipents.
     const initialBalances = await Promise.all(recipients.map(recipient => {
       return web3.eth.getBalance(recipient);
     }));
@@ -21,6 +22,7 @@ contract('SplitPayment', (accounts) => {
       return web3.eth.getBalance(recipient);
     }));
     recipients.forEach((_item, i) => {
+      // Balances are big numbers in web3
       const finalBalance = web3.utils.toBN(finalBalances[i]);
       const initialBalance = web3.utils.toBN(initialBalances[i]);
       assert(finalBalance.sub(initialBalance).toNumber() === amounts[i]);
